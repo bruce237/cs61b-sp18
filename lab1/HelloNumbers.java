6dcf30857762ef0a385cc9105b7b2af3b3e26d17
Hua-Dong Li <3583481181@qq.com>
2025-05-07 14:49:20 +0800

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,13 @@
+public class HelloNumbers{
+	public static voic main(String[] args){
+		int sum = 0;
+		int x = 0;
+        while (x < 10) {
+            sum = sum + x;
+	            System.out.print(sum + " ");
+	            x++;
+        }
+    }
+}
+
+
